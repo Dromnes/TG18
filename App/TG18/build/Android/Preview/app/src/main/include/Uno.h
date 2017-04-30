1cecf0a481d7cfc1f8df5eab7548b175aaac55c5
Kodemikkel <martin.krisknut@hotmail.com>
2017-04-30 03:22:12 +0200

    Fix untracked files

--- App/TG18/build/Android/Preview/app/src/main/include/Uno.h
+++
@@ -1,6 +0,0 @@
-// This file was generated based on '../../../Packages/UnoCore/0.47.13/backends/cplusplus/Uno.h'.
-// WARNING: Changes might be lost if you edit this file directly.
-#include <Uno/ObjectModel.h>
-//#if #(REFLECTION:Defined)
-#include <Uno/Reflection.h>
-//#endif
--- App/TG18/build/Android/Preview/cache/ux11/TG18.g
+++
@@ -1,5 +0,0 @@
-build/Android/Preview/cache/ux11/MainView.g.uno
-build/Android/Preview/cache/ux11/TBuilt.g.uno
-build/Android/Preview/cache/ux11/ControlPage.g.uno
-build/Android/Preview/cache/ux11/CustomButton.g.uno
-build/Android/Preview/cache/ux11/TG18.unoproj.g.uno
